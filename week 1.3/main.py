fd08e858cc86992400d1fc65d36f1513bac2a8a1
marshellon <marshellon@hotmail.com>
2023-07-05 10:40:25 +0200

    classes

--- week 1.3/main.py
+++ week 1.3/main.py
@@ -3,42 +3,49 @@ from avarageYear import AverageYear
 from averageMonth import AverageMonth
 import sys
 
-# getting all the input from the user
-print("set the data route and pick the data that ypu want to plot choices: 1 for year. 2 for month")
+class TemperaturePlotter:
+    def __init__(self, data_route, plot_type):
+        self.data_route = data_route
+        self.plot_type = plot_type
+
+    def plot_temperature(self):
+        if self.plot_type == "1":
+            av = AverageYear(self.data_route)
+            f = open(self.data_route, "r")
+            amount_of_lines = (len(f.readlines()) - 1) / 5
+            temp_temperture_list = [av.calculate_average() for _ in range(round(amount_of_lines))]
+
+            individual_temp_list_year = []
+            for temperatures in temp_temperture_list:
+                for temperature in temperatures:
+                    individual_temp_list_year.append(temperature)
+
+            plt.plot(individual_temp_list_year)
+            plt.grid()
+            plt.title("Average temperature per 5 years")
+            plt.show()
+
+        elif self.plot_type == "2":
+            av = AverageMonth(self.data_route)
+            f = open(self.data_route, "r")
+            amount_of_lines = (len(f.readlines()) - 1) / 5
+            temp_temperture_list = [av.calculate_average_month() for _ in range(round(amount_of_lines))]
+
+            individual_temp_list_month = []
+            for temperatures in temp_temperture_list:
+                for temperature in temperatures:
+                    individual_temp_list_month.append(temperature)
+
+            plt.plot(individual_temp_list_month)
+            plt.grid()
+            plt.title("Average temperature per month per 5 years")
+            plt.show()
+
+
+# Getting all the input from the user
 data_route = sys.argv[1]
 plot_type = sys.argv[2]
 
-if plot_type == "1":
-    av =  AverageYear(data_route)
-    f = open(data_route,"r")
-    amount_of_lines = (len(f.readlines()) -1) / 5
-    temp_temperture_list =  [av.calculate_average() for x in range(round(amount_of_lines))]
-
-    individual_temp_list_year = []
-    for tempertures in temp_temperture_list:
-        for temperture in tempertures:
-            individual_temp_list_year.append(temperture)
-
-    plt.plot(individual_temp_list_year)
-    plt.grid()
-    plt.title("average temperture per 5 years")
-    plt.show()
-
-
-# just check te sys input. if it is 2 than plot the months
-if plot_type == "2":
-    av =  AverageMonth(data_route)
-    f = open(data_route,"r")
-    amount_of_lines = (len(f.readlines()) -1) / 5 # the amount of cycles we have to do in the list comprehension
-    temp_temperture_list =  [av.calculate_average_month() for x in range(round(amount_of_lines))] # use list comprehension to get all the years
-
-    individual_temp_list_month = []
-    for tempertures in temp_temperture_list:
-        for temperture in tempertures:
-            individual_temp_list_month.append(temperture)
-
-    plt.plot(individual_temp_list_month)
-    plt.grid()
-    plt.title("average temperture per month per 5 years")
-    plt.show()
-
+# Create an instance of TemperaturePlotter and plot the temperature
+plotter = TemperaturePlotter(data_route, plot_type)
+plotter.plot_temperature()
